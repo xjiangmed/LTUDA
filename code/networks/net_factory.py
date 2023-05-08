9e475ac8e176a22091d997897f52c938cbc2b812
xjiangmed <xixijiang1998@gmail.com>
2023-05-08 15:10:08 +0800

    modified:   code/networks/net_factory.py

--- code/networks/net_factory.py
+++ code/networks/net_factory.py
@@ -5,7 +5,7 @@ def net_factory(net_type="unet"):
     if net_type == "unet":
         net = UNet(input_channel=1,num_class=4).cuda()
     elif net_type == "unet_proto":
-        net = UNet_proto(input_channel=1,num_class=4).cuda()
+        net = UNet_proto(input_channel=1,num_class=4, num_prototype=5).cuda()
     else:
         net = None 
     return net
