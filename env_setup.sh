4e09d401288db64351b20e6eca6ecb1df644dce2
xjiangmed <xixijiang1998@gmail.com>
2024-10-09 16:59:20 +0800

    new file:   env_setup.sh 	deleted:    requirements.txt

---
+++ env_setup.sh
@@ -0,0 +1,8 @@
+pip install torch==1.10.0+cu111 torchvision==0.11.0+cu111 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install tensorboard==2.9.0
+pip install tensorboardX==2.2
+pip install medpy==0.4.0
+pip install timm==0.6.12
+pip install einops==0.4.1
+pip install scikit-image==0.17.2
+pip install opencv-python==4.5.5.64
